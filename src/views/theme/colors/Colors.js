3143e2c2ebd286931795b4e5fd6bf660d28e3f42
Jose Moreno <joseangelmoreno1333@gmail.com>
2025-04-06 20:15:23 -0400

    Cambios: -Modulo colors se volvio departments. -en departments se añadio un formulario y tabla. -en el sidebar se cambio nombres y iconos . -en el body general se cambio el colo . -para complementar en departments se agg un archivo scss para editar botones , etc .

--- src/views/theme/colors/Colors.js
+++ src/views/theme/colors/Colors.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, createRef } from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import { CRow, CCol, CCard, CCardHeader, CCardBody } from '@coreui/react'
+import { CRow, CCol, CCard, CCardHeader, CCardBody,CButton,CModal,CModalBody,CModalHeader,CModalFooter,CForm, CFormInput, CFormLabel, CFormSelect,
+CTable,CTableHead,CTableRow,CTableHeaderCell,CTableBody,CTableDataCell} from '@coreui/react'
 import { rgbToHex } from '@coreui/utils'
 import { DocsLink } from 'src/components'
+import 'src/scss/colors.scss'
+import CIcon from '@coreui/icons-react'
+import { cilListNumbered, cilPlus } from '@coreui/icons'
 
 const ThemeView = () => {
   const [color, setColor] = useState('rgb(255, 255, 255)')
@@ -47,41 +51,172 @@ ThemeColor.propTypes = {
   className: PropTypes.string,
 }
 
+
+
+//------------------------------------------------------------------------
+
 const Colors = () => {
+
+  //estado para guardar deptm
+  //mejor dicho , es un arreglo que almacena los departamentos
+  const [departments, setDepartments] = useState([])  
+
+  //estado para la visibilidad del modal xd
+  const [modalVisible, setModalVisible] = useState(false) 
+  //almacena los datos del formulario , setFromdata funcion q se usa para actualizar los valores de formdata
+  const [formData, setFormData] = useState({  
+    name: '',                      
+    address: '',
+    phone: '',
+    email: '',
+    operational_status: '',
+  })
+  
+  //esta asociado al lo anterior , es lo que guarda los datos del formulario 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+  
+  // al presionar el boton save , este envia o guarda los datos -----------------------------------------------
+  const handleSubmit = () => {
+    if (!formData.name || !formData.address || !formData.phone || !formData.email || !formData.operational_status) {
+      //verifica si los campos estan vacios , y si los estan manda un mensaje 
+      alert('Please fill out all fields.')   
+      return
+    }
+
+    {/*si el correo no tiene alrroba y el .com manda alerta*/}
+    if (!formData.email.includes("@") || !formData.email.includes(".com")) {
+      alert('Please enter a valid email address.')
+      return
+    }
+
+        //departamentos :
+        setDepartments([...departments,formData]) //aqui se guardan los departamentos previamente llenados , osea del formulario
+        
+        setFormData({  //limpia el formulario
+          name: '',
+          address: '',
+          phone: '',
+          email: '',
+          operational_status: '',
+        })
+        setModalVisible(false) 
+  }
+  
+  
   return (
     <>
       <CCard className="mb-4">
         <CCardHeader>
-          Theme colors
-          <DocsLink href="https://coreui.io/docs/utilities/colors/" />
+          Departments
         </CCardHeader>
         <CCardBody>
-          <CRow>
-            <ThemeColor className="bg-primary">
-              <h6>Brand Primary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-secondary">
-              <h6>Brand Secondary Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-success">
-              <h6>Brand Success Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-danger">
-              <h6>Brand Danger Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-warning">
-              <h6>Brand Warning Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-info">
-              <h6>Brand Info Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-light">
-              <h6>Brand Light Color</h6>
-            </ThemeColor>
-            <ThemeColor className="bg-dark">
-              <h6>Brand Dark Color</h6>
-            </ThemeColor>
-          </CRow>
+          <CButton className="button-add" onClick={() => setModalVisible(true)}>Add <CIcon icon={cilPlus} /> </CButton> {/*cada q se da un click se abre el modal*/}
+          <CModal visible={modalVisible} onClose={() => setModalVisible(false)}>  {/*cada que se da un click se cierra el modal*/} 
+            <CModalHeader className='Modal-header'>Add New Department</CModalHeader>
+            <CModalBody className='Modal-body'>
+              <CForm>               {/*dentro del modal se encuentra el formulario*/}
+                <div className="mb-3">
+                <CFormLabel htmlFor="name">Department Name:</CFormLabel> {/*formulario para agregar el nombre del departamento*/}
+                <CFormInput 
+                  type='text'
+                  id='name'
+                  name='name'
+                  placeholder='Department Name'
+                  value={formData.name}
+                  onChange={handleInputChange}
+                ></CFormInput>
+                </div>
+                <div className="mb-3">
+                  <CFormLabel htmlFor="address">Address:</CFormLabel> {/*formulario para agregar la direccion del departamento*/}
+                  <CFormInput
+                    type='text'
+                    id='address'
+                    name='address'
+                    placeholder='Address'
+                    value={formData.address}
+                    onChange={handleInputChange}
+                  ></CFormInput>
+                </div>
+                <div className="mb-3">
+                  <CFormLabel htmlFor="phone">Phone:</CFormLabel> {/*formulario para agregar el telefono del departamento*/}
+                  <CFormInput
+                    type='tel'
+                    id='phone'
+                    name='phone'
+                    placeholder='Phone number'
+                    value={formData.phone}
+                    onChange={handleInputChange}
+                  ></CFormInput>
+                </div>
+                <div className="mb-3">
+                  <CFormLabel htmlFor="email">Email:</CFormLabel> {/*formulario para agregar el correo del departamento*/}
+                  <CFormInput
+                    type='email'
+                    id='email'
+                    name='email'
+                    placeholder='Email'
+                    value={formData.email}
+                    onChange={handleInputChange}
+                  ></CFormInput>
+                </div>
+                <div className="mb-3">
+                  <CFormLabel htmlFor="operational_status">Operational status:</CFormLabel> {/*formulario para agregar el estado del departamento*/}
+                  <CFormSelect
+                    id='operational_status'
+                    name='operational_status'
+                    value={formData.operational_status}
+                    onChange={handleInputChange}
+                  >
+                    <option value="">Select status</option>
+                    <option value="active">Active</option>
+                    <option value="inactive">Inactive</option>
+                  </CFormSelect>
+                </div>
+              </CForm>
+            </CModalBody>
+            <CModalFooter className='Modal-footer'> {/*footer del modal*/}
+              <CButton className='button-close' onClick={() => setModalVisible(false)}>Close</CButton> {/*boton para cerrar el modal*/}
+              <CButton className='button-save' onClick={handleSubmit}>Save</CButton> {/*boton para guardar los cambios*/}   
+            </CModalFooter>
+          </CModal>
+        </CCardBody>
+      </CCard>
+
+    {/*creo otro Ccard que contendra una tabla */}
+    {/*esta tabla almacena los departamentos guardados*/}
+
+      <CCard className="mb-4">
+        <CCardHeader>Management Departments</CCardHeader> 
+        <CCardBody>
+          <CTable striped hover>       {/*tabla con los departamentos*/}
+            <CTableHead>
+              <CTableRow>           {/*primera fila , en la cabezera , contiene los tipos de datos*/}
+              <CTableHeaderCell><CIcon icon={cilListNumbered}/> </CTableHeaderCell>
+              <CTableHeaderCell>Name</CTableHeaderCell>
+              <CTableHeaderCell>Address</CTableHeaderCell>
+              <CTableHeaderCell>Phone</CTableHeaderCell>
+              <CTableHeaderCell>Email</CTableHeaderCell>
+              <CTableHeaderCell>Status</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>       {/*ahora en el body de la tabla mostramos los datos almacenados*/}
+              {/*map es una funcion que se usa para recorrer un arreglo , en este caso es el de departments*/}
+              {/*el index , esta vaina es como un ciclo recorriendo un vector , department es el vector en cuestion y key es como un id , indentificador*/}
+              {departments.map((department, index) => (          
+                <CTableRow key={index}>
+                  <CTableDataCell>{index + 1}</CTableDataCell>
+                  <CTableDataCell>{department.name}</CTableDataCell>
+                  <CTableDataCell>{department.address}</CTableDataCell>
+                  <CTableDataCell>{department.phone}</CTableDataCell>
+                  <CTableDataCell>{department.email}</CTableDataCell>
+                  <CTableDataCell>{department.operational_status}</CTableDataCell>
+                </CTableRow>
+              ))}
+            </CTableBody>
+          </CTable>
         </CCardBody>
       </CCard>
     </>
