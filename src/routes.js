b0c6ac24b76bc8f71536683580fa53deeea73328
Jose Moreno <joseangelmoreno1333@gmail.com>
2025-06-21 17:33:27 -0400

    creo q asi se arreglo

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const Typography = React.lazy(() => import('./views/theme/typography/Typography'
 const users = React.lazy(() => import('src/views/management/Users/users.js'))
 const reports = React.lazy(() => import('./components/reports'))
 
-const Departments = React.lazy(() => import('src/views/management/departments/departments.js'))
+const Departments = React.lazy(() => import('../src/views/management/Departments/departments.js'))
 
 const Inventory = React.lazy(() => import('./components/Inventory.js'))
 
